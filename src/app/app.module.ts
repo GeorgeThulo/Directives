e91b4027271f4d26949cc86c360159bc4500de11
GeorgeThulo <Ofentsethulo@hotmail.com>
2024-07-10 10:06:50 +0200

    BasicDirective

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BasicHighlightDirectives } from './basic-highlight/basic-highlight.directives';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighlightDirectives
   ],
   imports: [
     BrowserModule,
